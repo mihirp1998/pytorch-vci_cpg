d46134d164689742c66831ba812711cbb9af67df
Mihir Prabhudesai <mihirp1998.mp@gmail.com>
2019-03-02 19:28:34 -0500

    init

---
+++ modules/conv_rnn.py
@@ -0,0 +1,170 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+from torch.autograd import Variable
+from torch.nn.modules.utils import _pair
+
+
+class ConvRNNCellBase(nn.Module):
+    def __repr__(self):
+        s = (
+            '{name}({input_channels}, {hidden_channels}, kernel_size={kernel_size}'
+            ', stride={stride}')
+        if self.padding != (0, ) * len(self.padding):
+            s += ', padding={padding}'
+        if self.dilation != (1, ) * len(self.dilation):
+            s += ', dilation={dilation}'
+        s += ', hidden_kernel_size={hidden_kernel_size}'
+        s += ')'
+        return s.format(name=self.__class__.__name__, **self.__dict__)
+
+
+class ConvLSTMCell(ConvRNNCellBase):
+    def __init__(self,
+                 input_channels,
+                 hidden_channels,
+                 kernel_size=3,
+                 stride=1,
+                 padding=0,
+                 dilation=1,
+                 hidden_kernel_size=1,
+                 bias=True):
+        super(ConvLSTMCell, self).__init__()
+        self.input_channels = input_channels
+        self.hidden_channels = hidden_channels
+
+        self.kernel_size = _pair(kernel_size)
+        self.stride = _pair(stride)
+        self.padding = _pair(padding)
+        self.dilation = _pair(dilation)
+
+        self.hidden_kernel_size = _pair(hidden_kernel_size)
+
+        hidden_padding = _pair(hidden_kernel_size // 2)
+
+        gate_channels = 4 * self.hidden_channels
+        self.conv_ih = nn.Conv2d(
+            in_channels=self.input_channels,
+            out_channels=gate_channels,
+            kernel_size=self.kernel_size,
+            stride=self.stride,
+            padding=self.padding,
+            dilation=self.dilation,
+            bias=bias)
+
+        self.conv_hh = nn.Conv2d(
+            in_channels=self.hidden_channels,
+            out_channels=gate_channels,
+            kernel_size=hidden_kernel_size,
+            stride=1,
+            padding=hidden_padding,
+            dilation=1,
+            bias=bias)
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.conv_ih.reset_parameters()
+        self.conv_hh.reset_parameters()
+
+    def forward(self, input, hidden):
+        hx, cx = hidden
+        gates = self.conv_ih(input) + self.conv_hh(hx)
+
+        ingate, forgetgate, cellgate, outgate = gates.chunk(4, 1)
+
+        ingate = F.sigmoid(ingate)
+        forgetgate = F.sigmoid(forgetgate)
+        cellgate = F.tanh(cellgate)
+        outgate = F.sigmoid(outgate)
+
+        cy = (forgetgate * cx) + (ingate * cellgate)
+        hy = outgate * F.tanh(cy)
+
+        return hy, cy
+
+
+
+class ConvLSTMCellTemp(ConvRNNCellBase):
+    def __init__(self,
+                 input_channels,
+                 hidden_channels,
+                 kernel_size=3,
+                 stride=1,
+                 padding=0,
+                 dilation=1,
+                 hidden_kernel_size=1,
+                 bias=True):
+        super(ConvLSTMCellTemp, self).__init__()
+        self.input_channels = input_channels
+        self.hidden_channels = hidden_channels
+        self.kernel_size = _pair(kernel_size)
+        self.stride = _pair(stride)
+        self.padding = _pair(padding)
+        self.dilation = _pair(dilation)
+        self.bias=bias
+        self.hidden_kernel_size = _pair(hidden_kernel_size)
+
+        self.hidden_padding = _pair(hidden_kernel_size // 2)
+
+        gate_channels = 4 * self.hidden_channels
+     
+        self.conv_ih = nn.Conv2d(
+            in_channels=self.input_channels,
+            out_channels=gate_channels,
+            kernel_size=self.kernel_size,
+            stride=self.stride,
+            padding=self.padding,
+            dilation=self.dilation,
+            bias=bias)
+
+        for param in self.conv_ih.parameters():
+            param.requires_grad = False
+
+        self.conv_hh = nn.Conv2d(
+            in_channels=self.hidden_channels,
+            out_channels=gate_channels,
+            kernel_size=self.hidden_kernel_size,
+            stride=1,
+            padding=self.hidden_padding,
+            dilation=1,
+            bias=bias)
+
+        for param in self.conv_hh.parameters():
+            param.requires_grad = False
+          
+
+    def forward(self, input ,conv_w_i, conv_w_h, hidden,batchsize):
+        hx, cx = hidden
+
+        conv_w_i = self.conv_ih.weight + conv_w_i
+        conv_w_h = self.conv_hh.weight + conv_w_h
+        #hx =hx.view(1,-1,hx.shape[2],hx.shape[3])
+        #print(conv_w_i.shape,"weird")
+        # gates = self.conv_ih(input) + self.conv_hh(hx)
+        # print("hx",input.shape,hx.shape)
+        #gate_input =  F.conv2d(input, conv_w_i, groups=self.batchsize,stride=self.stride,padding=self.padding)
+        #gate_hidden =  F.conv2d(hx, conv_w_h, groups=self.batchsize,stride=1,padding=self.hidden_padding)
+        #print("gate",gate_input.shape,gate_hidden.shape)
+        #gate_input = gate_input.view(self.batchsize,512,gate_input.shape[2],gate_input.shape[3])
+        #gate_hidden = gate_hidden.view(self.batchsize,512,gate_hidden.shape[2],gate_hidden.shape[3])
+        #print(gate_input.shape,gate_hidden.shape)
+        gate_input =  F.conv2d(input,conv_w_i,stride=self.stride,padding=self.padding)
+        gate_hidden = F.conv2d(hx,conv_w_h,stride=1,padding=self.hidden_padding)
+        #gate_input= batchConv2d(input,conv_w_i,batchsize,stride=self.stride,padding=self.padding,dilation=self.dilation,bias=self.bias)
+        #gate_hidden= batchConv2d(hx,conv_w_h,batchsize,stride=1,padding=self.hidden_padding,dilation=1,bias=self.bias)
+
+        gates = gate_input + gate_hidden
+
+        ingate, forgetgate, cellgate, outgate = gates.chunk(4, 1)
+
+        ingate = F.sigmoid(ingate)
+        forgetgate = F.sigmoid(forgetgate)
+        cellgate = F.tanh(cellgate)
+        outgate = F.sigmoid(outgate)
+
+        cy = (forgetgate * cx) + (ingate * cellgate)
+        hy = outgate * F.tanh(cy)
+
+        return hy, cy
+
---
+++ network.py
@@ -0,0 +1,250 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import Parameter, ParameterList
+from modules import ConvLSTMCell,ConvLSTMCellTemp, Sign
+
+
+class EncoderCell(nn.Module):
+    def __init__(self, v_compress, stack, fuse_encoder, fuse_level):
+        super(EncoderCell, self).__init__()
+
+        # Init.
+        self.v_compress = v_compress
+        self.fuse_encoder = fuse_encoder
+        self.fuse_level = fuse_level
+        if fuse_encoder:
+            print('\tEncoder fuse level: {}'.format(self.fuse_level))
+
+        # Layers.
+        self.conv = nn.Conv2d(
+            9 if stack else 3, 
+            64, 
+            kernel_size=3, stride=2, padding=1, bias=False)
+
+        for param in self.conv.parameters():
+            param.requires_grad = False
+
+        self.rnn1 = ConvLSTMCellTemp(
+            128 if fuse_encoder and v_compress else 64,
+            256,
+            kernel_size=3,
+            stride=2,
+            padding=1,
+            hidden_kernel_size=1,
+            bias=False)
+        # print(fuse_encoder,"fuse_encoder",v_compress,"v_compress")
+        self.rnn2 = ConvLSTMCellTemp(
+            ((384 if fuse_encoder and v_compress else 256) 
+             if self.fuse_level >= 2 else 256),
+            512,
+            kernel_size=3,
+            stride=2,
+            padding=1,
+            hidden_kernel_size=1,
+            bias=False)
+
+        self.rnn3 = ConvLSTMCellTemp(
+            ((768 if fuse_encoder and v_compress else 512) 
+             if self.fuse_level >= 3 else 512),
+            512,
+            kernel_size=3,
+            stride=2,
+            padding=1,
+            hidden_kernel_size=1,
+            bias=False)
+
+
+    def forward(self, input, hidden1, hidden2, hidden3,
+                unet_output1, unet_output2):
+
+        x = self.conv(input)
+        # Fuse
+        if self.v_compress and self.fuse_encoder:
+            x = torch.cat([x, unet_output1[2], unet_output2[2]], dim=1)
+
+        hidden1 = self.rnn1(x, hidden1)
+        x = hidden1[0]
+        # Fuse.
+        if self.v_compress and self.fuse_encoder and self.fuse_level >= 2:
+            x = torch.cat([x, unet_output1[1], unet_output2[1]], dim=1)
+
+        hidden2 = self.rnn2(x, hidden2)
+        x = hidden2[0]
+        # Fuse.
+        if self.v_compress and self.fuse_encoder and self.fuse_level >= 3:
+            x = torch.cat([x, unet_output1[0], unet_output2[0]], dim=1)
+
+        hidden3 = self.rnn3(x, hidden3)
+        x = hidden3[0]
+        return x, hidden1, hidden2, hidden3
+
+
+class Binarizer(nn.Module):
+    def __init__(self, bits):
+        super(Binarizer, self).__init__()
+        self.conv = nn.Conv2d(512, bits, kernel_size=1, bias=False)
+        for param in self.conv.parameters():
+            param.requires_grad = False
+        self.sign = Sign()
+
+    def forward(self, input):
+        feat = self.conv(input)
+        x = F.tanh(feat)
+        return self.sign(x)
+
+
+class DecoderCell(nn.Module):
+    def __init__(self, v_compress, shrink, bits, fuse_level):
+
+        super(DecoderCell, self).__init__()
+
+        # Init.
+        self.v_compress = v_compress
+        self.fuse_level = fuse_level
+        print('\tDecoder fuse level: {}'.format(self.fuse_level))
+
+        # Layers.
+        self.conv1 = nn.Conv2d(
+            bits, 512, kernel_size=1, stride=1, padding=0, bias=False)
+
+        for param in self.conv1.parameters():
+            param.requires_grad = False
+
+        self.rnn1 = ConvLSTMCellTemp(
+            512,
+            512,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            hidden_kernel_size=1,
+            bias=False)
+
+        self.rnn2 = ConvLSTMCellTemp(
+            (((128 + 256 // shrink * 2) if v_compress else 128) 
+             if self.fuse_level >= 3 else 128), #out1=256
+            512,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            hidden_kernel_size=1,
+            bias=False)
+
+        self.rnn3 = ConvLSTMCellTemp(
+            (((128 + 128//shrink*2) if v_compress else 128) 
+             if self.fuse_level >= 2 else 128), #out2=128
+            256,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            hidden_kernel_size=3,
+            bias=False)
+
+        self.rnn4 = ConvLSTMCellTemp(
+            (64 + 64//shrink*2) if v_compress else 64, #out3=64
+            128,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            hidden_kernel_size=3,
+            bias=False)
+
+        self.conv2 = nn.Conv2d(
+            32,
+            3, 
+            kernel_size=1, stride=1, padding=0, bias=False)
+
+        for param in self.conv2.parameters():
+            param.requires_grad = False
+
+    def forward(self, input, hidden1, hidden2, hidden3, hidden4,
+                unet_output1, unet_output2):
+
+        x = self.conv1(input)
+        hidden1 = self.rnn1(x, hidden1)
+
+        # rnn 2
+        x = hidden1[0]
+        x = F.pixel_shuffle(x, 2)
+
+        if self.v_compress and self.fuse_level >= 3:
+            x = torch.cat([x, unet_output1[0], unet_output2[0]], dim=1)
+
+        hidden2 = self.rnn2(x, hidden2)
+
+        # rnn 3
+        x = hidden2[0]
+        x = F.pixel_shuffle(x, 2)
+
+        if self.v_compress and self.fuse_level >= 2:
+            x = torch.cat([x, unet_output1[1], unet_output2[1]], dim=1)
+
+        hidden3 = self.rnn3(x, hidden3)
+
+        # rnn 4
+        x = hidden3[0]
+        x = F.pixel_shuffle(x, 2)
+
+        if self.v_compress:
+            x = torch.cat([x, unet_output1[2], unet_output2[2]], dim=1)
+
+        hidden4 = self.rnn4(x, hidden4)
+
+        # final
+        x = hidden4[0]
+        x = F.pixel_shuffle(x, 2)
+
+        x = F.tanh(self.conv2(x)) / 2
+        return x, hidden1, hidden2, hidden3, hidden4
+
+class HyperNetwork(nn.Module):
+
+    def __init__(self,num_vids):
+        super(HyperNetwork, self).__init__()
+        emb_size = num_vids
+        emb_dimension= 16
+
+        self.context_embeddings = nn.Embedding(emb_size, emb_dimension, sparse=False)
+        initrange = 0.5 / emb_dimension
+        self.context_embeddings.weight.data.uniform_(-initrange, initrange)
+        # self.z_dim = z_dim
+        self.enclayer =   [[64,9,3,3]]+[[1024,128,3,3]]+[[1024,256,1,1]]+[[2048,384,3,3]]+[[2048,512,1,1]]+[[2048,512,3,3]]+[[2048,512,1,1]]
+        self.declayer = [[512,8,1,1]]+[[2048,512,3,3]]+[[2048,512,1,1]]+[[2048,128,3,3]]+[[2048,512,1,1]]+[[1024,128,3,3]]+[[1024,256,3,3]]+[[512,128,3,3]]+[[512,128,3,3]]+[[3,32,1,1]]
+        self.binlayer=   [[8,512,1,1]]
+
+        # layer = np.array(declayer+enclayer+binlayer)
+
+        self.encoderWeights = nn.ParameterList([Parameter(torch.nn.init.xavier_normal_(torch.empty(emb_dimension, self.total(i)) )) for i in self.enclayer])
+        self.decoderWeights = nn.ParameterList([Parameter(torch.nn.init.xavier_normal_(torch.empty(emb_dimension, self.total(i)) )) for i in self.declayer])
+        self.binWeights = nn.ParameterList([Parameter(torch.nn.init.xavier_normal_(torch.empty(emb_dimension, self.total(i)) )) for i in self.binlayer])
+
+        #self.w1 =torch.nn.init.xavier_normal(self.w1)
+        self.encoderBias = nn.ParameterList([Parameter(torch.fmod(torch.zeros((self.total(i))),2)) for i in self.enclayer])
+        self.decoderBias = nn.ParameterList([Parameter(torch.fmod(torch.zeros((self.total(i))),2)) for i in self.declayer])
+        self.binBias = nn.ParameterList([Parameter(torch.fmod(torch.zeros((self.total(i))),2)) for i in self.binlayer])
+        #self.b1 =torch.nn.init.xavier_normal(self.b1)
+
+        #self.w2 = Parameter(torch.fmod(torch.randn((h,f)),2))
+        #self.b2 = Parameter(torch.fmod(torch.randn((f)),2))
+    def total(self,tensor_shape):
+        return tensor_shape[0]*tensor_shape[1]*tensor_shape[2]*tensor_shape[3]
+
+    def forward(self,id_num):
+        contextEmbed = self.context_embeddings(id_num)
+        #h_final= self.linear(contextEmbed)
+        #h_final = self.linear1(h_final)
+        enc_kernels = [(torch.matmul(contextEmbed,self.encoderWeights[i])  + self.encoderBias[i]).view(self.enclayer[i]) for i in range(len(self.encoderWeights))]
+        dec_kernels = [(torch.matmul(contextEmbed,self.decoderWeights[i])  + self.decoderBias[i]).view(self.declayer[i]) for i in range(len(self.decoderWeights))]
+        bin_kernels = [(torch.matmul(contextEmbed,self.binWeights[i])  + self.binBias[i]).view(self.binlayer[i]) for i in range(len(self.binWeights))]
+
+        # h_final = self.linear(contextEmbed
+        # print(enc_kernels[0].shape)
+
+        return enc_kernels,dec_kernels,bin_kernels[0]
+
+
+if __name__ == "__main__":
+    hn = HyperNetwork(10)
+    a,b,c = hn(torch.tensor(2))
+    print([i.shape for i in a],[i.shape for i in b],c.shape)
+
